c64a3ef626458bee0dba5022f61e9ccc7794b345
Jelena Drzaic <jelena.drzaic1@gmail.com>
2017-04-30 13:40:44 +0200

    initial commit

---
+++ SparseMatrix.m
@@ -0,0 +1,31 @@
+classdef SparseMatrix
+    % Sparse matrix format
+    
+    properties
+        n
+        m
+        mat
+    end
+    
+    methods
+        function obj = SparseMatrix(n, m)
+           obj.n = n;
+           obj.m = m;
+           % generated data - needs indexing overloading
+           obj.mat = cell(m, 1);
+        end
+        
+        function addCoordinateForWord(obj, wordInd, i, j)
+            currSize = length(obj.mat{wordInd})
+            obj.mat{wordInd}(currSize + 1) = i
+            obj.mat{wordInd}(currSize + 2) = j
+        end
+        
+        %function varargout = subsref(obj, S)
+        %   if length(S) == 1 && strcmp(S(1).type, '()')
+        %       S.sub
+        %   end
+        %end
+    end 
+end
+
