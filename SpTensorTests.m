4cdd6aee099a8d639ee0d92dec6209a1f7052d94
Jelena Drzaic <jelena.drzaic1@gmail.com>
2017-05-02 23:58:00 +0200

    test added

--- SpTensorTests.m
+++ SpTensorTests.m
@@ -9,8 +9,21 @@ normres = multi(normten, normmat, mode);
 for i = 1:m
     for j = 1:m
         for k = 1:m
-            normres(i, j, k)
-            spres(i, j, k)
+            assert(normres(i, j, k) == spres(i, j, k));
+        end
+    end
+end
+
+[spten, spmat, normten, normmat] = TestsResources.initializeMulti2(m);
+
+spres = TensorOperations.multi(spten, spmat, mode);
+normres = multi(normten, normmat, mode);
+
+mode = 3;
+
+for i = 1:m + 5
+    for j = 1:m + 5
+        for k = 1:m
             assert(normres(i, j, k) == spres(i, j, k));
         end
     end
--- TestsResources.m
+++ TestsResources.m
@@ -18,5 +18,30 @@ classdef TestsResources
                 normten(i, i, i) = val;
             end
         end
+        
+        function [spten, spmat, normten, normmat] = initializeMulti2(m)
+            spmat = sparse(m, m);
+            normmat = zeros(m, m);
+
+            for i = 1:m
+                val = rand();
+                spmat(i, i) = val;
+                normmat(i, i) = val;
+            end
+
+            spten = SpTensor(m + 5, m + 5, m);
+            normten = zeros(m + 5, m + 5,m);
+            for i = 1:m
+                val = rand();
+                spten(i, i, i) = val;
+                normten(i, i, i) = val;
+                val = rand();
+                spten(m + 2, m + 2, i) = val;
+                normten(m + 2, m + 2, i) = val;
+                val = rand();
+                spten(m + 4, m + 4, i) = val;
+                normten(m + 4, m + 4, i) = val;
+            end
+        end
     end
 end
